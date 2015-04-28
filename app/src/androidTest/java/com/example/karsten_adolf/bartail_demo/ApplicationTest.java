6c8a8a04ba19f953ab462c1016a88399ead7e337
René Glokan <rene.glockan@yahoo.de>
2015-04-28 12:38:22 +0200

    unit testing

--- app/src/androidTest/java/com/example/karsten_adolf/bartail_demo/ApplicationTest.java
+++ app/src/androidTest/java/com/example/karsten_adolf/bartail_demo/ApplicationTest.java
@@ -2,12 +2,22 @@ package com.example.karsten_adolf.bartail_demo;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+/*public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+}*/
+
+
+public class ApplicationTest extends InstrumentationTestCase{
+    public void logintest() throws Exception{
+        final int expected=1;
+        final int reality=5;
+        assertEquals(expected,reality);
     }
 }
