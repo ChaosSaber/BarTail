d2c382a1104b3f6c3e5c17f5cc976c30ba4057bb
René Glokan <rene.glockan@yahoo.de>
2015-05-08 14:59:21 +0200

    BAR-16 - Unit Test Login

---
+++ app/src/test/java/com/example/karsten_adolf/bartail_demo/LoginActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.karsten_adolf.bartail_demo;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by René on 08.05.2015.
+ */
+public class LoginActivityTest extends TestCase {
+
+    public void testOnclickCancel2() throws Exception {
+        //TODO testcase hinzufügen
+    }
+
+    public void testOnclickLogin2() throws Exception {
+
+    }
+}
