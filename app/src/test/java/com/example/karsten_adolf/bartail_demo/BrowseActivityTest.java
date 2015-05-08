d3c3604b33bf0a46c9163b5d02eb3f408d180042
René Glokan <rene.glockan@yahoo.de>
2015-05-08 14:56:37 +0200

    BAR-19 - UC Barsuche

---
+++ app/src/test/java/com/example/karsten_adolf/bartail_demo/BrowseActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.karsten_adolf.bartail_demo;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by René on 08.05.2015.
+ */
+public class BrowseActivityTest extends TestCase {
+
+}
